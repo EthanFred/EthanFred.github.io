91ec80bac7a7851a899fbe5fe739b6165d5b427d
EthanFred <etfr2117@colorado.edu>
2025-10-21 10:40:32 -0600

    ica10

---
+++ ica/ica10/ica10.js
@@ -0,0 +1,65 @@
+let triviaBtn = document.querySelector("#js-new-quote").
+addEventListener('click', newTrivia);
+
+let answerBtn = document.querySelector("#js-tweet").
+addEventListener('click', newAnswer);
+
+
+let current = {
+    question: "",
+    answer: ""
+};
+
+
+
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+
+
+
+async function newTrivia()
+{
+    //console.log("Hi");
+
+    try {
+        const response = await fetch(endpoint);
+        if(!response.ok)
+        {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        //console.log(json);
+        
+        
+        current.question = json["question"];
+        current.answer = json["answer"];
+        //console.log(current.question);
+        //console.log(current.answer);
+
+        displayTrivia(json["question"]);
+
+        
+    }
+    catch (err){
+        console.log(err)
+        alert('Failed to get new trivia');
+    }
+}
+
+
+function displayTrivia(question)
+{
+    const questionText = document.querySelector('#js-quote-text');
+    questionText.textContent = question;
+    const answerText = document.querySelector('#js-answer-text');
+    answerText.textContent = "";
+}
+
+function newAnswer()
+{
+    const answerText = document.querySelector('#js-answer-text');
+    answerText.textContent = current.answer;
+}
+
+newTrivia();
